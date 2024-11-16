c611687ff8409d3fdbaad84cad14fd041fb9c22e
SebastianHahsler <167276710+SebastianHahsler@users.noreply.github.com>
2024-11-16 10:49:23 -0600

    Update Node.java
    Changed the Node.java to handle prices as a string rather than a double

--- Node.java
+++ Node.java
@@ -2,18 +2,16 @@ public class Node {
     String productId;
     String name;
     String category;
-    double minPrice;
-    double maxPrice;
+    String price; // Store price as a string
     Node left, right, parent;
     boolean isRed;
 
-    public Node(String productId, String name, String category, double minPrice, double maxPrice) {
+    public Node(String productId, String name, String category, String price) {
         this.productId = productId;
         this.name = name;
         this.category = category;
-        this.minPrice = minPrice;
-        this.maxPrice = maxPrice;
-        this.isRed = true; // New nodes are always red
+        this.price = price;
+        this.isRed = true; // New nodes are red initially
         this.left = this.right = this.parent = null;
     }
 }
